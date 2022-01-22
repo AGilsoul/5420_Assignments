58eea10c908dd87ead5df8c23ae3e92f1afee7ce
Alex Gilsoul <54417319+AGilsoul@users.noreply.github.com>
2022-01-21 21:49:40 -0700

    Final Upload

---
+++ Assignment1/cmake-build-debug/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/agils/Desktop/5420 Advanced Algorithms/Assignment1/src/alg_graphs.cpp" "C:/Users/agils/Desktop/5420 Advanced Algorithms/Assignment1/cmake-build-debug/CMakeFiles/main.dir/src/alg_graphs.cpp.obj"
+  "C:/Users/agils/Desktop/5420 Advanced Algorithms/Assignment1/src/alg_stopwatch.cpp" "C:/Users/agils/Desktop/5420 Advanced Algorithms/Assignment1/cmake-build-debug/CMakeFiles/main.dir/src/alg_stopwatch.cpp.obj"
+  "C:/Users/agils/Desktop/5420 Advanced Algorithms/Assignment1/src/alg_strings.cpp" "C:/Users/agils/Desktop/5420 Advanced Algorithms/Assignment1/cmake-build-debug/CMakeFiles/main.dir/src/alg_strings.cpp.obj"
+  "C:/Users/agils/Desktop/5420 Advanced Algorithms/Assignment1/src/main.cpp" "C:/Users/agils/Desktop/5420 Advanced Algorithms/Assignment1/cmake-build-debug/CMakeFiles/main.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Assignment1/cmake-build-debug/CMakeFiles/tester.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tester.dir/src/alg_graphs.cpp.obj"
+  "CMakeFiles/tester.dir/src/alg_stopwatch.cpp.obj"
+  "CMakeFiles/tester.dir/src/alg_strings.cpp.obj"
+  "CMakeFiles/tester.dir/test/tester.cpp.obj"
+  "libtester.dll.a"
+  "tester.exe"
+  "tester.exe.manifest"
+  "tester.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
